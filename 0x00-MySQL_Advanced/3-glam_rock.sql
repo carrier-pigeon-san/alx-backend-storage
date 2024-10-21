2aeba95415c35910a3c4da5f4a20ef93373dfc15
Sandy Obare <obaresandy@gmail.com>
2024-10-21 14:06:59 +0300

    3-glam_rock.sql initial commit

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- A script that lists all bands with Glam rock as their main style ranked by their longeivity
+SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
