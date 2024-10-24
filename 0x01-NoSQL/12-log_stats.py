052416a9e2995794fbe6a506c14fd62ddd4b562a
Sandy Obare <obaresandy@gmail.com>
2024-10-24 04:46:11 +0300

    12-log_stats.py update: syntax update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,15 +3,17 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient(host='localhost', port=27017)
-db = client.logs
-col = db.nginx
 
-print(f"{col.count_documents({})} logs")
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    print(f"\tmethod {method}: {col.count_documents({'method': method})}")
-print("{} status check".format(
-    col.count_documents({'method': 'GET', 'path': '/status'})
-    ))
+if __name__ == "__main__":
+    client = MongoClient(host='localhost', port=27017)
+    db = client.logs
+    col = db.nginx
+
+    print(f"{col.count_documents({})} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print(f"\tmethod {method}: {col.count_documents({'method': method})}")
+    print("{} status check".format(
+        col.count_documents({'method': 'GET', 'path': '/status'})
+        ))
