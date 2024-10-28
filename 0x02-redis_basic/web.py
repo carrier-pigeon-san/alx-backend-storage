aa27682ca7a4eeadd9f7eec4f3c54ed4a59334f2
Sandy Obare <obaresandy@gmail.com>
2024-10-28 15:09:58 +0300

    web.py count_Calls decorator function

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,14 +2,25 @@
 """An expiring web cache and tracker"""
 import redis
 import requests
+from functools import wraps
 from typing import Callable
 
-redis = redis.Redis()
+r = redis.Redis()
+
+def count_calls(method: Callable) -> Callable:
+    """Counts how many times get_page has been called."""
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        """Wrapper function"""
+        key = f"count:{url}"
+        r.incr(key)
+        r.expire(key, 10)
+        page = method(url)
+        return page
+    return wrapper
 
 
+@count_calls
 def get_page(url: str) -> str:
     """Uses requests to get the content of a webpage."""
-    key = f"count:{url}"
-    redis.incr(key)
-    redis.expire(key, 10)
     return requests.get(url).text
